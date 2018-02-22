569573e43b44bf2de6ed21c803b0fe80d4758f6c
Griffen Lawrence <griffenlawre@gmail.com>
2018-02-22 13:29:57 -0500

    Added APIClient to begin to gather data from the api for the app

---
+++ Simple Weather/API/Weather.swift
@@ -0,0 +1,45 @@
+//
+//  Weather.swift
+//  Simple Weather
+//
+//  Created by Griffen Lawrence on 2/22/18.
+//  Copyright © 2018 Griffen Lawrence. All rights reserved.
+//
+
+import Foundation
+
+class weather: Codable {
+    let forecast: Forecast
+    
+}
+
+struct Forecast: Codable {
+    let forecastText: ForecastText
+    
+    private enum CodingKeys: String, CodingKey {
+        case forecastText = "txt_forecast"
+    }
+}
+
+struct ForecastText: Codable {
+    let date: String
+    let forecastDays: [ForecastDay]
+    
+    private enum CodingKeys: String, CodingKey {
+        case date
+        case forecastDays = "forecastday"
+    }
+}
+
+struct ForecastDay: Codable {
+    let iconUrl: String
+    let day: String
+    let description: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case iconUrl = "icon_url"
+        case day = "title"
+        case description = "fcttext"
+    }
+    
+}
